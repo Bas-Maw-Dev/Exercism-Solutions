2d8b9c487b04213d81e2f42a874dca6098d868a3
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-11-04 12:13:13 +0000

    all tests pass

--- ruby/eliuds-eggs/eliuds_eggs.rb
+++ ruby/eliuds-eggs/eliuds_eggs.rb
@@ -1,5 +1,10 @@
 class EliudsEggs
   def self.egg_count(eggs)
-    0
+    decimal_to_binary(eggs).count('1').to_i
+  end
+
+  def self.decimal_to_binary(number, binary=[])
+    return binary.join if number == 0
+    decimal_to_binary(number / 2, [number % 2] + binary)
   end
 end
