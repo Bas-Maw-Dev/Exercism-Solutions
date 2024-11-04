9730c975467ab81aab4f8ef74f8bd4bf338c2980
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-11-04 14:29:53 +0000

    Ruby: raindrops - Refactor

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,22 +1,19 @@
 class Raindrops
   def self.convert(number)
-    case number
-    when ->(number) { number % 3 == 0 && number % 5 == 0 && number % 7 == 0 }
-      return 'PlingPlangPlong'
-    when ->(number) { number % 3 == 0 && number % 5 == 0 }
-      return 'PlingPlang'
-    when ->(number) { number % 3 == 0 && number % 7 == 0 }
-      return 'PlingPlong'
-    when ->(number) { number % 5 == 0 && number % 7 == 0 }
-      return 'PlangPlong'
-    when ->(number) { number % 3 == 0 }
-      return 'Pling'
-    when ->(number) { number % 5 == 0 }
-      return 'Plang'
-    when ->(number) { number % 7 == 0 }
-      return 'Plong'
-    else
-      number.to_s
-    end
+    converted = multiple_of_three(number) + multiple_of_five(number) + multiple_of_seven(number)
+    converted.empty? ? number.to_s : converted
   end
+
+  def self.multiple_of_three(number)
+    number % 3 == 0 ? 'Pling' : ''
+  end
+
+  def self.multiple_of_five(number)
+    number % 5 == 0 ? 'Plang' : ''
+  end
+
+  def self.multiple_of_seven(number)
+    number % 7 == 0 ? 'Plong' : ''
+  end
+
 end
